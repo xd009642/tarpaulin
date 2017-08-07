1b2897d5db09268fb36e3b069b157ac7ad8f7e9b
xd009642 <danielmckenna93@gmail.com>
2017-08-07 21:05:48 +0100

    Updated travis install script.
    Travis install script updated after new release build published.

--- travis-install.sh
+++ travis-install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -sL https://github.com/xd009642/tarpaulin/releases/download/0.3.11/cargo-tarpaulin-0.3.11-travis.tar.gz | tar xvz -C $HOME/.cargo/bin
+curl -sL https://github.com/xd009642/tarpaulin/releases/download/0.3.12/cargo-tarpaulin-0.3.12-travis.tar.gz | tar xvz -C $HOME/.cargo/bin
