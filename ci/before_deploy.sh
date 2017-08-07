83ce9472384f3867a2a2a5711eac9831b1714645
xd009642 <danielmckenna93@gmail.com>
2017-08-07 08:59:23 +0100

    Started github releases integration
    Added travis setup to deploy travis builds to github releases for faster downloads as suggested in issue #29.
    Will come into effect in the next version.
    
    Also, need to update README.md with the new process for downloading the release.

---
+++ ci/before_deploy.sh
@@ -0,0 +1,33 @@
+# This script takes care of building your crate and packaging it for release
+
+set -ex
+
+main() {
+    local src=$(pwd) \
+          stage=
+
+    case $TRAVIS_OS_NAME in
+        linux)
+            stage=$(mktemp -d)
+            ;;
+        osx)
+            stage=$(mktemp -d -t tmp)
+            ;;
+    esac
+
+    test -f Cargo.lock || cargo generate-lockfile
+
+    # TODO Update this to build the artifacts that matter to you
+    cargo build --release 
+
+    # TODO Update this to package the right artifacts
+    cp target/release/zxcvbn-rs $stage/
+
+    cd $stage
+    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-travis.tar.gz *
+    cd $src
+
+    rm -rf $stage
+}
+
+main
