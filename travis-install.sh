2c26a87cb45324289196280ac4b852793b8916de
xd009642 <danielmckenna93@gmail.com>
2017-08-08 16:45:27 +0100

    Updated to version 0.3.13

--- travis-install.sh
+++ travis-install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -sL https://github.com/xd009642/tarpaulin/releases/download/0.3.12/cargo-tarpaulin-0.3.12-travis.tar.gz | tar xvz -C $HOME/.cargo/bin
+curl -sL https://github.com/xd009642/tarpaulin/releases/download/0.3.13/cargo-tarpaulin-0.3.13-travis.tar.gz | tar xvz -C $HOME/.cargo/bin
