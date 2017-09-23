63c34d607cd5e151a78c2f7e85e7a08776762584
xd009642 <danielmckenna93@gmail.com>
2017-09-23 15:51:59 +0100

    Release 0.5.0.
    Update install script and Cargo manifest.

--- travis-install.sh
+++ travis-install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -sL https://github.com/xd009642/tarpaulin/releases/download/0.4.2/cargo-tarpaulin-0.4.2-travis.tar.gz | tar xvz -C $HOME/.cargo/bin
+curl -sL https://github.com/xd009642/tarpaulin/releases/download/0.5.0/cargo-tarpaulin-0.5.0-travis.tar.gz | tar xvz -C $HOME/.cargo/bin
