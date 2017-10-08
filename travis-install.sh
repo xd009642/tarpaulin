d3e1c6a1c5e3bc755e9217abf0465f9c2026f194
xd009642 <danielmckenna93@gmail.com>
2017-10-08 16:42:17 +0100

    Releasing 0.5.1

--- travis-install.sh
+++ travis-install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -sL https://github.com/xd009642/tarpaulin/releases/download/0.5.0/cargo-tarpaulin-0.5.0-travis.tar.gz | tar xvz -C $HOME/.cargo/bin
+curl -sL https://github.com/xd009642/tarpaulin/releases/download/0.5.1/cargo-tarpaulin-0.5.1-travis.tar.gz | tar xvz -C $HOME/.cargo/bin
