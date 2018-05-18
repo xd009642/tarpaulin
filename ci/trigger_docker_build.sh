b64f2130b673c55c33352f5c067a02f16ab585c7
xd009642 <danielmckenna93@gmail.com>
2018-05-18 17:09:34 +0100

    Update trigger_docker_build.sh
    Made docker token pirvate.

--- ci/trigger_docker_build.sh
+++ ci/trigger_docker_build.sh
@@ -1,1 +1,1 @@
-curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/xd009642/tarpaulin/trigger/e50f3cfc-8c09-4ca1-bdd7-d4b8789e820a/
+curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/xd009642/tarpaulin/trigger/$DOCKER_TOKEN/
