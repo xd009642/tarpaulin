7bebbdd3ab8f6e28c8bbe139ebc00b7a24882b8b
xd009642 <danielmckenna93@gmail.com>
2019-03-07 21:13:28 +0000

    Shitty printout

--- src/report/coveralls.rs
+++ src/report/coveralls.rs
@@ -133,6 +133,7 @@ pub fn export(coverage_data: &TraceMap, config: &Config) -> Result<(), RunError>
         if config.debug {
             if let Ok(text) = serde_json::to_string(&report) {
                 info!("Attempting to write coveralls report to coveralls.json");
+                println!("{}", text);
                 let _ = fs::write("coveralls.json", text);
             } else {
                 warn!("Failed to serialise coverage report");
