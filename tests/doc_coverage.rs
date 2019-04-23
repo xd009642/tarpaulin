04367c9edd49be07a578b221724ae108c3ab38ed
Daniel Kolsoi <ThaDan64@gmail.com>
2019-04-23 10:35:52 -0700

    Make tests pathing relative to Cargo manifest (#222)
    * Make tests pathing relative to Cargo manifest
    * Also fix path building in doc_coverage.rs
    * Also use manifest dir in line_coverage.rs
    * Refactor test path code into helper function

--- tests/doc_coverage.rs
+++ tests/doc_coverage.rs
@@ -1,3 +1,4 @@
+use crate::utils::get_test_path;
 use cargo_tarpaulin::config::{Config, RunType};
 use cargo_tarpaulin::launch_tarpaulin;
 use std::env;
@@ -8,12 +9,9 @@ fn doc_test_coverage() {
     let mut config = Config::default();
     config.verbose = true;
     config.test_timeout = Duration::from_secs(60);
-    let mut test_dir = env::current_dir().unwrap();
-    test_dir.push("tests");
-    test_dir.push("data");
-    test_dir.push("doc_coverage");
-    env::set_current_dir(test_dir.clone()).unwrap();
-    config.manifest = test_dir.clone();
+    let test_dir = get_test_path("doc_coverage");
+    env::set_current_dir(&test_dir).unwrap();
+    config.manifest = test_dir;
     config.manifest.push("Cargo.toml");
 
     config.run_types = vec![RunType::Doctests];
