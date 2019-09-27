103d1a355c3473d7a5c5a649bee237061eb3ccbc
atanunq <atanas.yankov98@gmail.com>
2019-09-27 19:21:19 +0300

    Change grammar

--- src/lib.rs
+++ src/lib.rs
@@ -347,7 +347,7 @@ pub fn report_coverage(config: &Config, result: &TraceMap) -> Result<(), RunErro
                 }
                 _ => {
                     return Err(RunError::OutFormat(
-                        "Format currently unsupported".to_string(),
+                        "Output format is currently not supported!".to_string(),
                     ));
                 }
             }
