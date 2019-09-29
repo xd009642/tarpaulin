7533f7d9af45a779a6d0985b5b5e0a22f7c91a2d
xd009642 <danielmckenna93@gmail.com>
2019-09-29 21:37:07 +0100

    More futures
    Now fails all the time

--- tests/data/futures/src/lib.rs
+++ tests/data/futures/src/lib.rs
@@ -5,8 +5,17 @@ pub fn a() {
     futures::executor::ThreadPool::new();
 }
 
-
 #[test]
 pub fn b() {
+    futures::executor::ThreadPool::new();
+}
+
+#[test]
+pub fn c() {
+    futures::executor::ThreadPool::new();
+}
+
+#[test]
+pub fn d() {
     futures::executor::ThreadPool::new();
 }
