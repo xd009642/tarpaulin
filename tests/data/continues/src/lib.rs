c798c7e6dbcb4043a5ad2e9a1e6b5ce955e6ace4
camchenry <camchenry@users.noreply.github.com>
2019-10-09 10:05:58 -0400

    Run rustfmt on continue expr tests

--- tests/data/continues/src/lib.rs
+++ tests/data/continues/src/lib.rs
@@ -13,18 +13,16 @@ fn test_loop_continue_no_label_no_value() {
     }
 }
 
-
 #[test]
 fn test_for_continues() {
     for _i in 0..10 {
         continue;
     }
 
-
-    let mut v = vec!();
+    let mut v = vec![];
     for i in 0..10 {
-        if i%2 == 0 {
-            continue
+        if i % 2 == 0 {
+            continue;
         } else {
             v.push(i);
         }
@@ -33,19 +31,16 @@ fn test_for_continues() {
     assert_eq!(v, [1, 3, 5, 7, 9]);
 }
 
-
 #[test]
 fn test_for_continue_with_labels() {
-
     'for1: for _i in 0..10 {
         continue 'for1;
     }
 
-
-    let mut v = vec!();
+    let mut v = vec![];
     'for2: for i in 0..10 {
-        if i%2 == 0 {
-            continue 'for2
+        if i % 2 == 0 {
+            continue 'for2;
         } else {
             v.push(i);
         }
@@ -53,27 +48,30 @@ fn test_for_continue_with_labels() {
 
     assert_eq!(v, [1, 3, 5, 7, 9]);
 
-    let mut v = vec!();
+    let mut v = vec![];
     'x: for x in 0..4 {
         'y: for y in 0..4 {
-            if x % 2 == 0 { continue 'x; }
-            if y % 2 == 0 { continue 'y; }
+            if x % 2 == 0 {
+                continue 'x;
+            }
+            if y % 2 == 0 {
+                continue 'y;
+            }
             v.push((x, y));
         }
     }
     assert_eq!(v, [(1, 1), (1, 3), (3, 1), (3, 3)]);
 }
 
-
 #[test]
 fn test_while_continues() {
     let mut x = 0;
 
-    let mut v = vec!();
+    let mut v = vec![];
     while x < 10 {
         x += 1;
-        if x%2 == 0 {
-            continue
+        if x % 2 == 0 {
+            continue;
         } else {
             v.push(x);
         }
@@ -86,10 +84,10 @@ fn test_while_continues() {
 fn test_while_continues_with_labels() {
     let mut x = 0;
 
-    let mut v = vec!();
+    let mut v = vec![];
     'while1: while x < 10 {
         x += 1;
-        if x%2 == 0 {
+        if x % 2 == 0 {
             continue 'while1;
         } else {
             v.push(x);
@@ -99,14 +97,18 @@ fn test_while_continues_with_labels() {
     assert_eq!(v, [1, 3, 5, 7, 9]);
 
     let mut x = 0;
-    let mut v = vec!();
+    let mut v = vec![];
     'x: while x < 4 {
         x += 1;
         let mut y = 0;
         'y: while y < 4 {
             y += 1;
-            if x % 2 == 0 { continue 'x; }
-            if y % 2 == 0 { continue 'y; }
+            if x % 2 == 0 {
+                continue 'x;
+            }
+            if y % 2 == 0 {
+                continue 'y;
+            }
             v.push((x, y));
         }
     }
