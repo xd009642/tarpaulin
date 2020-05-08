220280a0aa51d4b99352170b51d23442d99868b0
xd009642 <danielmckenna93@gmail.com>
2020-05-08 14:52:57 +0100

    Fix span ranges and implement tests

--- src/branching/mod.rs
+++ src/branching/mod.rs
@@ -1,5 +1,6 @@
 use std::collections::{BTreeMap, HashMap};
 use std::path::{Path, PathBuf};
+use syn::spanned::Spanned;
 use syn::*;
 
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
@@ -64,12 +65,17 @@ pub struct Branches {
 
 impl From<&ExprIf> for Branches {
     fn from(expr: &ExprIf) -> Self {
-        let mut ranges = vec![];
+        let mut then = LineRange::from(&expr.then_branch);
+        then.end -= 1;
+        let mut ranges = vec![then];
         let mut else_block = expr.else_branch.as_ref().map(|x| x.1.clone());
         let mut implicit_default = else_block.is_none();
         while let Some(el) = else_block {
             let mut lr = LineRange::from(&el);
             if let Expr::If(ref i) = *el {
+                then = LineRange::from(&i.then_branch);
+                then.end -= 1;
+                ranges.push(then);
                 else_block = i.else_branch.as_ref().map(|x| x.1.clone());
                 implicit_default = else_block.is_none();
                 if let Some(s) = &else_block {
@@ -107,18 +113,22 @@ impl From<&ExprMatch> for Branches {
 
 impl From<&ExprForLoop> for Branches {
     fn from(expr: &ExprForLoop) -> Self {
+        let mut range = LineRange::from(&expr.body);
+        range.start = expr.expr.span().end().line + 1;
         Self {
             implicit_default: true,
-            ranges: vec![expr.body.clone().into()],
+            ranges: vec![range],
         }
     }
 }
 
 impl From<&ExprWhile> for Branches {
     fn from(expr: &ExprWhile) -> Self {
+        let mut range = LineRange::from(&expr.body);
+        range.start = expr.cond.span().end().line + 1;
         Self {
             implicit_default: true,
-            ranges: vec![expr.body.clone().into()],
+            ranges: vec![range],
         }
     }
 }
@@ -195,20 +205,145 @@ mod tests {
         assert_eq!(branches.ranges.len(), 2);
         assert_eq!(branches.ranges[0], LineRange::new(2, 4));
         assert_eq!(branches.ranges[1], LineRange::new(4, 6));
+        let source = "fn foo(x: i32) {
+            if x > 0 {
+                println!(\"BOO\");
+            } else {
+                todo!()
+            }
+        }";
+        let parser = parse_file(source).unwrap();
+        let func = &parser.items[0];
+        let mut branches = None;
+        if let Item::Fn(func) = func {
+            for stmt in &func.block.stmts {
+                match stmt {
+                    Stmt::Expr(e) => {
+                        if let Expr::If(i) = e {
+                            branches = Some(Branches::from(i));
+                        }
+                    }
+                    Stmt::Semi(e, _) => {
+                        if let Expr::If(i) = e {
+                            branches = Some(Branches::from(i));
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
+        let branches = branches.unwrap();
+        assert!(!branches.implicit_default);
+        assert_eq!(branches.ranges.len(), 2);
+        assert_eq!(branches.ranges[0], LineRange::new(2, 4));
+        assert_eq!(branches.ranges[1], LineRange::new(4, 7));
     }
 
     #[test]
     fn match_branches() {
-        todo!()
+        let source = "fn foo(x: i32) {
+            match 4 {
+                0 => {
+                    todo!();
+                },
+                1 => {
+                    todo!();
+                },
+                _ => {},
+            }
+        }";
+        let parser = parse_file(source).unwrap();
+        let func = &parser.items[0];
+        let mut branches = None;
+        if let Item::Fn(func) = func {
+            for stmt in &func.block.stmts {
+                match stmt {
+                    Stmt::Expr(e) => {
+                        if let Expr::Match(m) = e {
+                            branches = Some(Branches::from(m));
+                        }
+                    }
+                    Stmt::Semi(e, _) => {
+                        if let Expr::Match(m) = e {
+                            branches = Some(Branches::from(m));
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
+        let branches = branches.unwrap();
+        assert!(!branches.implicit_default);
+        assert_eq!(branches.ranges.len(), 3);
+        assert_eq!(branches.ranges[0], LineRange::new(3, 6));
+        assert_eq!(branches.ranges[1], LineRange::new(6, 9));
+        assert_eq!(branches.ranges[2], LineRange::new(9, 10));
     }
 
     #[test]
     fn for_branches() {
-        todo!()
+        let source = "fn foo(x: i32) {
+            for i in 0..1 {
+                todo!();
+            }
+        }";
+        let parser = parse_file(source).unwrap();
+        let func = &parser.items[0];
+        let mut branches = None;
+        if let Item::Fn(func) = func {
+            for stmt in &func.block.stmts {
+                match stmt {
+                    Stmt::Expr(e) => {
+                        if let Expr::ForLoop(f) = e {
+                            branches = Some(Branches::from(f));
+                        }
+                    }
+                    Stmt::Semi(e, _) => {
+                        if let Expr::ForLoop(f) = e {
+                            branches = Some(Branches::from(f));
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
+        let branches = branches.unwrap();
+        assert!(branches.implicit_default);
+        assert_eq!(branches.ranges.len(), 1);
+        assert_eq!(branches.ranges[0], LineRange::new(3, 5));
     }
 
     #[test]
     fn while_branches() {
-        todo!()
+        let source = "fn foo(x: i32) {
+            let mut i = 0;
+            while i < 10 {
+                i+=1;
+            }
+        }";
+        let parser = parse_file(source).unwrap();
+        let func = &parser.items[0];
+        let mut branches = None;
+        if let Item::Fn(func) = func {
+            for stmt in &func.block.stmts {
+                match stmt {
+                    Stmt::Expr(e) => {
+                        if let Expr::While(w) = e {
+                            branches = Some(Branches::from(w));
+                        }
+                    }
+                    Stmt::Semi(e, _) => {
+                        if let Expr::While(w) = e {
+                            branches = Some(Branches::from(w));
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
+        let branches = branches.unwrap();
+        assert!(branches.implicit_default);
+        assert_eq!(branches.ranges.len(), 1);
+        assert_eq!(branches.ranges[0], LineRange::new(4, 6));
     }
 }
