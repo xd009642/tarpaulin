175b84c770019970ed852e8c78801ea1f4d2289f
xd009642 <danielmckenna93@gmail.com>
2021-03-03 11:32:29 +0400

    Rustc now errors on unsupported attrs on rustdoc
    Fix dependencies so this no longer bothers my CI

--- tests/data/all_test_types/benches/bench_fn.rs
+++ tests/data/all_test_types/benches/bench_fn.rs
@@ -1,11 +1,6 @@
 use all_test_types::only_bench::*;
-use criterion::{criterion_group, criterion_main, Criterion};
 
-fn check_speed(c: &mut Criterion) {
-    c.bench_function("some_fn", |b| {
-        b.iter(|| only_ran_in_benches(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
-    });
+#[test]
+fn check_speed() {
+    only_ran_in_benches(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 }
-
-criterion_group!(benches, check_speed);
-criterion_main!(benches);
