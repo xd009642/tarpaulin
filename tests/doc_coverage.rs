3760d22dd08289575e08863e8a8478142a672638
xd009642 <danielmckenna93@gmail.com>
2021-05-18 23:04:29 +0100

    Propagate CARGO_MANIFEST_DIR to doctest

--- tests/doc_coverage.rs
+++ tests/doc_coverage.rs
@@ -5,6 +5,25 @@ use std::env;
 use std::time::Duration;
 
 #[test]
+fn doc_test_env() {
+    let mut config = Config::default();
+    config.force_clean = false;
+    config.test_timeout = Duration::from_secs(60);
+    let test_dir = get_test_path("doctest_env");
+    env::set_current_dir(&test_dir).unwrap();
+    config.manifest = test_dir;
+    config.manifest.push("Cargo.toml");
+
+    config.run_types = vec![RunType::Doctests];
+
+    let (res, ret) = launch_tarpaulin(&config, &None).unwrap();
+
+    assert_eq!(ret, 0);
+    assert!(res.total_covered() > 0);
+    assert_eq!(res.total_covered(), res.total_coverable());
+}
+
+#[test]
 fn doc_test_coverage() {
     let mut config = Config::default();
     config.verbose = true;
