b13f8167080ad54705c3874b196677995e6bb40c
Daniel Eades <danieleades@hotmail.com>
2022-01-04 07:00:50 +0000

    add additional test cases for 'no_run'

--- tests/data/doctest_norun/src/lib.rs
+++ tests/data/doctest_norun/src/lib.rs
@@ -9,3 +9,22 @@
 pub fn do_something() {
     todo!()
 }
+
+
+/// This function doesn't do much
+///
+/// ```rust,no_run
+/// panic!("Don't run me")
+/// ```
+pub fn do_something_else() {
+    todo!()
+}
+
+/// This function doesn't do much
+///
+/// ```rust no_run
+/// panic!("Don't run me")
+/// ```
+pub fn do_something_again() {
+    todo!()
+}
