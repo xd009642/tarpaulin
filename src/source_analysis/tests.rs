15a3a8a2b49fce09b5064dbb6f19056b97d733c7
xd009642 <danielmckenna93@gmail.com>
2022-09-11 16:42:08 +0100

    Fix ignore filtering test

--- src/source_analysis/tests.rs
+++ src/source_analysis/tests.rs
@@ -386,7 +386,8 @@ fn filter_macros() {
 
 #[test]
 fn filter_tests() {
-    let config = Config::default();
+    let mut config = Config::default();
+    config.set_ignore_tests(false);
     let mut igconfig = Config::default();
     igconfig.set_ignore_tests(true);
 
@@ -473,7 +474,8 @@ fn filter_test_utilities() {
     assert!(lines.ignore.contains(&Lines::Line(3)));
     assert!(lines.ignore.contains(&Lines::Line(4)));
 
-    let config = Config::default();
+    let mut config = Config::default();
+    config.set_ignore_tests(false);
 
     let ctx = Context {
         config: &config,
