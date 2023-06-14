d6bc0ffd9db4e95bf3dc5844f8a307cce8640e36
xd009642 <danielmckenna93@gmail.com>
2023-06-14 23:32:40 +0100

    Fix test

--- tests/test_types.rs
+++ tests/test_types.rs
@@ -91,10 +91,10 @@ fn all_targets_coverage() {
 
     for f in res.files() {
         let f_name = f.file_name().unwrap().to_str().unwrap();
-        if f_name.contains("example") {
-            assert!(res.covered_in_path(f) > 0);
-        } else {
+        if f_name.contains("doc") {
             assert_eq!(res.covered_in_path(f), 0);
+        } else {
+            assert!(res.covered_in_path(f) > 0);
         }
     }
 }
