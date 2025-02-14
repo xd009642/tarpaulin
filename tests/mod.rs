14f9330f6eb7a739bd45b5acafd27723314206c6
xd009642 <danielmckenna93@gmail.com>
2025-02-14 19:28:51 +0900

    Remove torch from mac tests

--- tests/mod.rs
+++ tests/mod.rs
@@ -165,7 +165,7 @@ fn lets_coverage() {
 }
 
 #[test]
-//#[cfg_attr(not(target_os="linux"), ignore)] // TODO So there are linker issues I can't adequately diagnose myself in windows
+#[cfg_attr(target_os="macos", ignore)]
 #[cfg(not(tarpaulin))]
 fn picking_up_shared_objects() {
     // Need a project which downloads a shared object to target folder and uses build script to set
