7d41879c8c53082728db0aa6584e147276919b27
xd009642 <danielmckenna93@gmail.com>
2025-05-30 11:29:56 +0900

    Fix example

--- tests/data/torch_test/src/lib.rs
+++ tests/data/torch_test/src/lib.rs
@@ -2,7 +2,7 @@ use tch::Tensor;
 
 #[test]
 fn test_dummy() {
-    let t = Tensor::of_slice(&[3, 1, 4, 1, 5]);
+    let t = Tensor::from_slice(&[3, 1, 4, 1, 5]);
     let t = t * 2;
     t.print();
 }
